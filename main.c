dae508d7f3aa31fe9626edc9c1b134e29f78a430
Luc Joffily Ribas <luc.joffily.ribas@gmail.com>
2023-05-23 18:05:02 -0300

    fixed list (but problems with print)

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
 #include <pthread.h>
 
 #define MULTIPLIER 10
 #define MIN_SIZE 1
+#define STRUCT_SIZE 24
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 
@@ -15,7 +16,7 @@ typedef enum {
 
 
 typedef struct MemMetadata {
-    size_t size;
+    unsigned int size;
     MemStatus status;
     struct MemMetadata* prev;
     struct MemMetadata* next;
@@ -23,41 +24,19 @@ typedef struct MemMetadata {
 } MemMetadata;
 
 
-static MemMetadata* head = NULL;
+static MemMetadata* globalHead = NULL;
 
 
 void printMemBlocks(MemMetadata* head) {
-    MemMetadata* current;
-    for (current = head; current != NULL; current = current->next){
-        printf("metadata at: %p size: %ld \n", current, current->size);
+    int count = 0;
+    for (MemMetadata* current = head; current != NULL; current = current->next){
+        printf("Chunk %d:\n", count);
+        printf("metadata at: %p\n", current);
+        printf("size: %d\n", current->size);
+        printf("MemStatus: %d\n", current->status);
+        printf("Next: %p\n\n", current->next);
+        count++;
     }
-}
-
-
-MemMetadata* requestMoreMemory(MemMetadata* lastChunk, size_t size){
-    void* brkPoint = sbrk(0);
-
-    size_t requestedMemory = MULTIPLIER * (size + sizeof(MemMetadata));
-    assert(sbrk(requestedMemory) != (void*) -1 && "Could not alloc more memory");
-
-    MemMetadata* newChunk = brkPoint;
-    newChunk->size = requestedMemory - sizeof(MemMetadata);
-    newChunk->status = AVAILABLE;
-    newChunk->prev = lastChunk;
-    newChunk->next = NULL;
-
-    // first call to bbmalloc
-    if (lastChunk == NULL)
-        lastChunk = newChunk;
-    else
-        lastChunk->next = newChunk;
-
-    return lastChunk;
-}
-
-
-int isLastChunk(MemMetadata* chunk){
-    return (chunk != NULL) && (chunk->next == NULL);
 }
 
 
@@ -65,30 +44,73 @@ void splitChunk(MemMetadata* chunk, size_t size){
     assert(chunk != NULL && "Cannot split a NULL chunk");
 
     MemMetadata* newChunk = (MemMetadata*) chunk->memBegin + size;
-    newChunk->size = chunk->size - (size + sizeof(MemMetadata));
+    newChunk->size = chunk->size - (size + STRUCT_SIZE);
     newChunk->status = chunk->size < MIN_SIZE ? TOO_SMALL : AVAILABLE;
     newChunk->prev = chunk;
-    newChunk->next = NULL;
+    newChunk->next = chunk->next;
 
+    if (chunk->next != NULL) chunk->next->prev = newChunk;
+
+    chunk->status = UNAVAILABLE;
     chunk->size = size;
     chunk->next = newChunk;
+
 }
 
+
+MemMetadata* requestMoreMemory(MemMetadata* lastChunk, size_t size){
+    void* brkPoint = sbrk(0);
+
+    size_t requestedMemory = MULTIPLIER * (size + STRUCT_SIZE);
+    assert(sbrk(requestedMemory) != (void*) -1 && "Could not alloc more memory");
+
+    if (lastChunk != NULL && lastChunk->size == 0) lastChunk = lastChunk->prev;
+    MemMetadata* newChunk = brkPoint;
+    newChunk->size = requestedMemory - STRUCT_SIZE;
+    newChunk->status = AVAILABLE;
+    newChunk->prev = lastChunk;
+    newChunk->next = NULL;
+
+    if (lastChunk != NULL)
+        lastChunk->next = newChunk;
+
+    splitChunk(newChunk, size);
+
+
+    return newChunk;
+}
+
+int isLastChunk(MemMetadata* chunk){
+    return (chunk != NULL) && (chunk->next == NULL);
+}
 
 MemMetadata* findChunk(MemMetadata* head, size_t size){
     MemMetadata* current;
     for (current = head; current != NULL; current = current->next){
-        if (current->size >= size + sizeof(MemMetadata) && current-> status == AVAILABLE){
+        if (current-> status == AVAILABLE && current->size >= size + STRUCT_SIZE){
             splitChunk(current, size);
-            current->status = UNAVAILABLE;
             break; // first fit
         }
         // if the last chunk is not large enough to store the size requested by
         // the user, request more memory. This will add a new chunk to the
         // linked list, which will be allocated in the next iteration
-        if (isLastChunk(current)) requestMoreMemory(current, size);
+        if (isLastChunk(current)) current = requestMoreMemory(current, size);
     }
     return current;
+}
+
+void* bbmalloc(size_t size) {
+    MemMetadata* chunk;
+
+    // first call to bbmalloc
+    if (globalHead == NULL) {
+        globalHead = requestMoreMemory(globalHead, size);
+        chunk = globalHead;
+    }
+        // next calls to bbmalloc
+    else chunk = findChunk(globalHead, size);
+
+    return chunk->memBegin;
 }
 
 void mergeChunkPrev(MemMetadata* chunk){
@@ -98,7 +120,7 @@ void mergeChunkPrev(MemMetadata* chunk){
             (chunk->prev->status == AVAILABLE || chunk->prev->status == TOO_SMALL)) {
 
         // increment the size of the merged chunk
-        chunk->prev->size += (chunk->size + sizeof(MemMetadata));
+        chunk->prev->size += (chunk->size + STRUCT_SIZE);
 
         // remove the freed chunk from the linked list
         MemMetadata* temp = chunk->next;
@@ -115,7 +137,7 @@ void mergeChunkNext(MemMetadata* chunk){
             (chunk->next->status == AVAILABLE || chunk->next->status == TOO_SMALL)) {
 
         // increment the size of the merged chunk
-        chunk->size += (chunk->next->size + sizeof(MemMetadata));
+        chunk->size += (chunk->next->size + STRUCT_SIZE);
 
         // remove the freed chunk from the linked list
         MemMetadata* temp = chunk->next;
@@ -126,42 +148,46 @@ void mergeChunkNext(MemMetadata* chunk){
 }
 
 void bbfree(void* ptr){
-    assert(ptr >= (void*) head && ptr <= sbrk(0) &&
-            "Attempted to free memory outside of allocated region");
+    assert(ptr >= (void*) globalHead && ptr <= sbrk(0) &&
+           "Attempted to free memory outside of allocated region");
 
     // pointers point to the beginning of the data itself, so the related
     // metadata is directly behind it hence the pointer arithmetic below
     // FIXME: the program segfaults when chunkToFree is accessed
-    MemMetadata* chunkToFree = (MemMetadata *) ptr - sizeof(MemMetadata);
+    MemMetadata* chunkToFree = ptr - STRUCT_SIZE;
     chunkToFree->status = AVAILABLE;
+    printf("size: %d\n", chunkToFree->size);
 
-    // merge adjacent chunks that are either available or too small
-    mergeChunkPrev(chunkToFree);
-    mergeChunkNext(chunkToFree);
-}
-
-
-void* bbmalloc(size_t size) {
-    MemMetadata* chunk;
-
-    // first call to bbmalloc
-    if (head == NULL) {
-        chunk = requestMoreMemory(head, size);
-        splitChunk(chunk, size);
-    }
-    // next calls to bbmalloc
-    else chunk = findChunk(head, size);
-
-    return chunk;
+//    // merge adjacent chunks that are either available or too small
+//    mergeChunkPrev(chunkToFree);
+//    mergeChunkNext(chunkToFree);
 }
 
 
 int main(int argc, char* argv[]) {
-    for (int i = 0; i != 1000; i++){
-        int* a = bbmalloc(sizeof(int));
-        *a = 10;
-        printf("%p->%d\n", a, *a);
-        printf("%p\n", sbrk(0));
-    }
+//    int *a[12];
+//    for (int i = 0; i != 12; i++) {
+//        a[i] = bbmalloc(sizeof(int));
+//        *(a[i]) = i;
+//        printf("a\n");
+////        printf("sbrk: %p\n", sbrk(0));
+////        printf("head: %p\n", globalHead);
+//
+//    }
+    int *a = (int *) bbmalloc(3*sizeof(int));
+    a[0] = 1;
+    a[1] = 8;
+    a[2] = 3;
+    printMemBlocks(globalHead);
+    int *b = (int *) bbmalloc(sizeof(int));
+
+//    printMemBlocks(globalHead);
+//    bbfree(a);
+
+    double* k = (double *) bbmalloc(sizeof(double));
+//    bbfree(k);
+//    bbfree(a);
+//    bbfree(b);
+
     return 0;
 }
